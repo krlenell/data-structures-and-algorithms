db3610784b8fb8b0c31729723a05dc65575a7a9a
Kevin Lenell <krlenell@gmail.com>
2020-10-17 16:55:52 -0700

    edited add and prepend.  creaded contains()

--- javascript/src/LinkedList.ts
+++ javascript/src/LinkedList.ts
@@ -2,27 +2,57 @@ import LinkedListNode from './LinkedListNode'
 
 class LinkedList <T> {
   head: null | LinkedListNode<T>
+  tail: null | LinkedListNode<T>
   length: number
 
   constructor(){
     this.head = null
+    this.tail = null
     this.length = 0
   }
 
-  addNode(value: T): void{
+  add(value: T): LinkedList<T>{
     const node = new LinkedListNode(value)
-    let current : LinkedListNode<T>
-    if(this.head === null){
+    if(this.tail === null){
       this.head = node
+      this.tail = node
+      return this
     } else{
-      current = this.head
-      while(current.next){
-        current= current.next
-      }
-      current.next = this.head
+      this.tail.next = node
+      this.tail = node
     }
     this.length++
+    return this
+  }
+
+  prepend(value: T): LinkedList<T>{
+    const node = new LinkedListNode(value)
+    if(this.head === null){
+      this.head = node
+      this.tail = node
+      return this
+    } else {
+      node.next = this.head
+      this.head = node
+    }
+    this.length++
+    return this
+  }
+
+  contains(value: T): boolean{
+    if(!this.head){
+      return false
+    }
+    let current = this.head
+    while(current.next){
+      if(current.value === value){
+        return true
+      }
+    }
+    return false
   }
 
 
 }
+
+export default LinkedList
