585b20857e8d8424997eb452c6fc71add937a7f6
Kevin Lenell <krlenell@gmail.com>
2020-11-06 19:23:16 -0800

    fixed TS implementation

--- javascript/src/BenHashTable.ts
+++ javascript/src/BenHashTable.ts
@@ -2,23 +2,30 @@ function hashStringtoInt(s: string, tableSize: number): number {
   let hash = 17;
 
   for (let i = 0; i < s.length; i++) {
-    hash = (7 * hash * s.charCodeAt(i)) % tableSize;
+    hash = (17 * hash * s.charCodeAt(i)) % tableSize;
   }
 
   return hash;
 }
 
-class HashTable<T> {
-  table = new Array(100);
+type keyPair = [string, unknown];
 
-  setItem = (key: string, value: string) => {
+class HashTable<T> {
+  table = new Array(17);
+
+  setItem = (key: string, value: unknown) => {
     const index = hashStringtoInt(key, this.table.length);
-    this.table[index] = value;
+    const pair: keyPair = [key, value];
+    if (this.table[index]) {
+      this.table[index].push(pair);
+    } else {
+      this.table[index] = [pair];
+    }
   };
 
   getItem = (key: string) => {
     const index = hashStringtoInt(key, this.table.length);
-    return this.table[index];
+    return this.table[index].find((x) => x[0] === key)[1];
   };
 }
 
@@ -26,5 +33,6 @@ const myTable = new HashTable();
 myTable.setItem("firstName", "bob");
 myTable.setItem("lastName", "tim");
 myTable.getItem("lastName");
+console.log(myTable.table);
 console.log(myTable.getItem("firstName"));
 console.log(myTable.getItem("lastName"));
