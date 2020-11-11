e82f0a3bf70aaabf5a2f21eeaaaa76404e8fc3f3
Kevin Lenell <krlenell@gmail.com>
2020-11-10 21:00:53 -0800

    added resize function

--- javascript/src/NewHashTable.ts
+++ javascript/src/NewHashTable.ts
@@ -31,6 +31,10 @@ class NewHashTable<T> {
     } else {
       this.keys.push(key);
       this.items++;
+      const loadFactor = this.table.length / this.items;
+      if (loadFactor > 0.8) {
+        this.resize(this.table.length * 2);
+      }
       const index = hashFunction(key, this.table.length);
       const pair: [string, T] = [key, value];
       if (this.table[index]) {
@@ -42,13 +46,32 @@ class NewHashTable<T> {
   };
 
   //resize
+  resize = (length: number) => {
+    const resizedTable = new Array<Array<[string, T]>>(length);
+    this.table.forEach((item) => {
+      if (item) {
+        item.forEach(([key, value]) => {
+          const index = hashFunction(key, length);
+          const pair: [string, T] = [key, value];
+          if (resizedTable[index]) {
+            resizedTable[index].push(pair);
+          } else {
+            resizedTable[index] = [pair];
+          }
+        });
+      }
+    });
+    this.table = resizedTable;
+  };
 }
 
 const newMyTable = new NewHashTable();
 newMyTable.set("jeff", 100);
 newMyTable.set("steve", "hello");
+console.log(newMyTable.table.length);
 newMyTable.set("jim", "99");
 newMyTable.set("bill", "69 dude");
+console.log(newMyTable.table.length);
 newMyTable.set("chris", "krisss");
 newMyTable.set("chris", "oh hi");
 console.log(newMyTable.items);
