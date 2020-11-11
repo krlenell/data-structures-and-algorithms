fc509ab8ada203b9e3cd1fd179f54b1d45681a7f
Kevin Lenell <krlenell@gmail.com>
2020-11-10 20:39:38 -0800

    duplicate keys now get values replaced properly

--- javascript/src/NewHashTable.ts
+++ javascript/src/NewHashTable.ts
@@ -1,5 +1,3 @@
-import { table } from "console";
-
 function hashFunction(key: string, tableSize: number): number {
   let hash = 0;
   for (let i = 0; i < key.length; i++) {
@@ -10,7 +8,9 @@ function hashFunction(key: string, tableSize: number): number {
 }
 
 class NewHashTable<T> {
-  table = new Array<Array<[string, T]>>(7);
+  table = new Array<Array<[string, T]>>(1);
+  items = 0;
+  keys: string[] = [];
 
   get = (key: string) => {
     const index = hashFunction(key, this.table.length);
@@ -18,19 +18,40 @@ class NewHashTable<T> {
   };
 
   set = (key: string, value: T) => {
-    const index = hashFunction(key, this.table.length);
-    const pair: [string, T] = [key, value];
-    if (this.table[index]) {
-      this.table[index].push(pair);
+    //checks if key already exists
+    if (this.keys.includes(key)) {
+      const index = hashFunction(key, this.table.length);
+      //checks if array is worth searching
+      if (this.table[index].length === 1) {
+        this.table[index][0][1] = value;
+      } else {
+        const indx = this.table[index].findIndex((x) => x[0] === key);
+        this.table[index][indx][1] = value;
+      }
     } else {
-      this.table[index] = [pair];
+      this.keys.push(key);
+      this.items++;
+      const index = hashFunction(key, this.table.length);
+      const pair: [string, T] = [key, value];
+      if (this.table[index]) {
+        this.table[index].push(pair);
+      } else {
+        this.table[index] = [pair];
+      }
     }
   };
 
   //resize
 }
 
-const myTable = new NewHashTable();
-myTable.set("jeff", 100);
-console.log(myTable);
-console.log(myTable.get("jeff"));
+const newMyTable = new NewHashTable();
+newMyTable.set("jeff", 100);
+newMyTable.set("steve", "hello");
+newMyTable.set("jim", "99");
+newMyTable.set("bill", "69 dude");
+newMyTable.set("chris", "krisss");
+newMyTable.set("chris", "oh hi");
+console.log(newMyTable.items);
+console.log(newMyTable.table);
+console.log(newMyTable.get("jeff"));
+console.log(newMyTable.get("chris"));
